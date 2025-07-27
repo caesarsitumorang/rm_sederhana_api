ff9063d8906e1c72333ab15f9c3a14fc1215d387
caesarsitumorang <caesarsitumorang16@gmail.com>
2025-07-27 23:51:48 +0700

    vercel 9

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: '103.163.138.21',
-  user: 'tempakod_caesar',
-  password: 'caesar',
-  database: 'tempakod_db_rm_sederhana',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
