f677b72dd4de69fc3dc9ad2c1b7ec72b66032021
caesarsitumorang <caesarsitumorang16@gmail.com>
2025-07-27 23:25:48 +0700

    vercel 5

--- db/config.js
+++ db/config.js
@@ -1,14 +1,18 @@
-// db/config.js
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const db = mysql.createConnection({
+  host: '103.163.138.21',
+  user: 'tempakod_caesar',
+  password: 'caesar',
+  database: 'tempakod_db_rm_sederhana',
 });
 
-module.exports = pool;
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = db;
